73db74aa70310f137039be017990efee44f4035e
Hansongli <125562851+HansongProgramming@users.noreply.github.com>
2025-01-31 18:18:42 +0800

    changes to blender.py, adding actors (brush, canvas) now works with button click. no seen issues with simulation as of the moment

--- blender.py
+++ blender.py
@@ -1,98 +1,158 @@
 import bpy
 
-class BLOOD_SPLATTER_PT_Panel(bpy.types.Panel):
-    bl_label = "Blood Splatter Simulator"
-    bl_idname = "BLOOD_SPLATTER_PT_Panel"
+# Function to add the brush and canvas planes
+def add_actors(self, context):
+    # Create the "brush" collection if it doesn't exist
+    if "brush" not in bpy.data.collections:
+        bpy.data.collections.new("brush")
+
+    # Get the brush collection
+    brush_collection = bpy.data.collections["brush"]
+
+    # Link the brush collection to the scene collection for visibility in the Outliner
+    if brush_collection.name not in bpy.context.scene.collection.children:
+        bpy.context.scene.collection.children.link(brush_collection)
+
+
+    # Add Brush Plane code
+    bpy.ops.mesh.primitive_plane_add(enter_editmode=False, align='WORLD', location=(0, 0, 0), scale=(1, 1, 1))
+    brush_plane = bpy.context.object
+
+    # Move the Brush Plane to the "brush" collection
+    brush_collection.objects.link(brush_plane)
+
+    bpy.ops.transform.translate(value=(0, 0, 2.70914), orient_type='GLOBAL', orient_matrix=((1, 0, 0), (0, 1, 0), (0, 0, 1)), orient_matrix_type='GLOBAL', constraint_axis=(False, False, True), mirror=False, use_proportional_edit=False, proportional_edit_falloff='SMOOTH', proportional_size=1, use_proportional_connected=False, use_proportional_projected=False, snap=False, snap_elements={'INCREMENT'}, use_snap_project=False, snap_target='CLOSEST', use_snap_self=True, use_snap_edit=True, use_snap_nonedit=True, use_snap_selectable=False, release_confirm=True)
+    bpy.ops.transform.rotate(value=2.1342, orient_axis='Y', orient_type='GLOBAL', orient_matrix=((1, 0, 0), (0, 1, 0), (0, 0, 1)), orient_matrix_type='GLOBAL', constraint_axis=(False, True, False), mirror=False, use_proportional_edit=False, proportional_edit_falloff='SMOOTH', proportional_size=1, use_proportional_connected=False, use_proportional_projected=False, snap=False, snap_elements={'INCREMENT'}, use_snap_project=False, snap_target='CLOSEST', use_snap_self=True, use_snap_edit=True, use_snap_nonedit=True, use_snap_selectable=False, release_confirm=True)
+    bpy.ops.transform.rotate(value=3.14159, orient_axis='X', orient_type='GLOBAL', orient_matrix=((1, 0, 0), (0, 1, 0), (0, 0, 1)), orient_matrix_type='GLOBAL', constraint_axis=(True, False, False), mirror=False, use_proportional_edit=False, proportional_edit_falloff='SMOOTH', proportional_size=1, use_proportional_connected=False, use_proportional_projected=False, snap=False, snap_elements={'INCREMENT'}, use_snap_project=False, snap_target='CLOSEST', use_snap_self=True, use_snap_edit=True, use_snap_nonedit=True, use_snap_selectable=False)
+    bpy.ops.object.particle_system_add()
+    bpy.ops.object.modifier_add(type='SUBSURF')
+    bpy.data.particles["ParticleSettings"].render_type = 'OBJECT'
+    bpy.context.object.modifiers["Subdivision"].levels = 8
+    bpy.context.object.modifiers["Subdivision"].render_levels = 8
+    bpy.ops.transform.resize(value=(0.145423, 0.145423, 0.145423), orient_type='GLOBAL', orient_matrix=((1, 0, 0), (0, 1, 0), (0, 0, 1)), orient_matrix_type='GLOBAL', mirror=False, use_proportional_edit=False, proportional_edit_falloff='SMOOTH', proportional_size=1, use_proportional_connected=False, use_proportional_projected=False, snap=False, snap_elements={'INCREMENT'}, use_snap_project=False, snap_target='CLOSEST', use_snap_self=True, use_snap_edit=True, use_snap_nonedit=True, use_snap_selectable=False)
+    bpy.data.particles["ParticleSettings"].display_size = 0.03
+    bpy.data.particles["ParticleSettings"].normal_factor = 3
+    bpy.data.particles["ParticleSettings"].factor_random = 3
+    bpy.data.particles["ParticleSettings"].frame_end = 5
+    bpy.data.particles["ParticleSettings"].count = 400
+
+    bpy.ops.mesh.primitive_ico_sphere_add(radius=1, enter_editmode=False, align='WORLD', location=(0, 0, 0), scale=(1, 1, 1))
+    bpy.ops.object.shade_smooth()
+    bpy.ops.transform.translate(value=(4.83279, 0, 0), orient_type='GLOBAL', orient_matrix=((1, 0, 0), (0, 1, 0), (0, 0, 1)), orient_matrix_type='GLOBAL', constraint_axis=(True, False, False), mirror=False, use_proportional_edit=False, proportional_edit_falloff='SMOOTH', proportional_size=1, use_proportional_connected=False, use_proportional_projected=False, snap=False, snap_elements={'INCREMENT'}, use_snap_project=False, snap_target='CLOSEST', use_snap_self=True, use_snap_edit=True, use_snap_nonedit=True, use_snap_selectable=False, release_confirm=True)
+    bpy.ops.transform.resize(value=(0.106316, 0.106316, 0.106316), orient_type='GLOBAL', orient_matrix=((1, 0, 0), (0, 1, 0), (0, 0, 1)), orient_matrix_type='GLOBAL', mirror=False, use_proportional_edit=False, proportional_edit_falloff='SMOOTH', proportional_size=1, use_proportional_connected=False, use_proportional_projected=False, snap=False, snap_elements={'INCREMENT'}, use_snap_project=False, snap_target='CLOSEST', use_snap_self=True, use_snap_edit=True, use_snap_nonedit=True, use_snap_selectable=False)
+
+    plane_object = bpy.data.objects["Plane"]
+    bpy.context.view_layer.objects.active = plane_object  # Set the active object to Plane
+    bpy.data.particles["ParticleSettings"].instance_object = bpy.data.objects["Icosphere"]
+
+    bpy.ops.object.modifier_add(type='DYNAMIC_PAINT')
+    bpy.context.object.modifiers["Dynamic Paint"].ui_type = 'BRUSH'
+    bpy.ops.dpaint.type_toggle(type='BRUSH')
+
+    bpy.context.object.modifiers["Dynamic Paint"].brush_settings.paint_source = 'PARTICLE_SYSTEM'
+    bpy.context.object.modifiers["Dynamic Paint"].brush_settings.particle_system = bpy.data.objects["Plane"].particle_systems["ParticleSystem"]
+    bpy.context.object.modifiers["Dynamic Paint"].brush_settings.solid_radius = 0.03
+    bpy.context.object.modifiers["Dynamic Paint"].brush_settings.smooth_radius = 0
+    bpy.context.object.modifiers["Dynamic Paint"].brush_settings.paint_color = (0.8, 0.0171646, 0)
+
+    # Add Canvas Plane code (the existing canvas setup)
+    bpy.ops.mesh.primitive_plane_add(size=2, enter_editmode=False, align='WORLD', location=(0, 0, 0))
+    canvas = bpy.context.object
+
+    subsurf = canvas.modifiers.new(name="Subdivision", type='SUBSURF')
+    subsurf.levels = 8
+    subsurf.render_levels = 8
+    subsurf.subdivision_type = 'SIMPLE'
+
+    dp = canvas.modifiers.new(name="Dynamic Paint", type='DYNAMIC_PAINT')
+    bpy.ops.dpaint.type_toggle(type='CANVAS')
+
+    if "brush" in bpy.data.collections:
+        dp.canvas_settings.canvas_surfaces["Surface"].brush_collection = bpy.data.collections["brush"]
+
+    dp.canvas_settings.canvas_surfaces["Surface"].init_color_type = 'COLOR'
+
+    material = bpy.data.materials.new(name="DynamicPaintMaterial")
+    material.use_nodes = True
+    canvas.data.materials.append(material)
+
+    node_tree = material.node_tree
+    nodes = node_tree.nodes
+    links = node_tree.links
+
+    attribute_node = nodes.new(type="ShaderNodeAttribute")
+    attribute_node.location = (-300, 100)
+    attribute_node.attribute_name = "dp_paintmap"
+
+    principled_bsdf = nodes.get("Principled BSDF")
+    if principled_bsdf:
+        links.new(attribute_node.outputs["Color"], principled_bsdf.inputs["Base Color"])
+
+    dp.canvas_settings.canvas_surfaces["Surface"].use_spread = True
+    dp.canvas_settings.canvas_surfaces["Surface"].use_drip = True
+
+    for area in bpy.context.screen.areas:
+        if area.type == 'PROPERTIES':
+            override = {'area': area, 'object': canvas}
+
+            bpy.ops.dpaint.output_toggle(override, output='A')
+            bpy.ops.dpaint.output_toggle(override, output='B')
+
+            print("✔ Successfully toggled Output A & B!")
+            break
+    else:
+        print("⚠ Could not find the PROPERTIES area! bpy.ops.dpaint.output_toggle() may fail.")
+
+# Function to play the simulation (animation)
+def play_simulation(self, context):
+    bpy.ops.screen.animation_play()
+
+# Panel UI class
+class SimplePanel(bpy.types.Panel):
+    bl_label = "Simulation Controls"
+    bl_idname = "PT_SimulationControls"
     bl_space_type = 'VIEW_3D'
     bl_region_type = 'UI'
-    bl_category = 'BloodSim'
+    bl_category = "Simulation"
 
     def draw(self, context):
         layout = self.layout
-        layout.operator("object.add_actors", text="Add Actors", icon='MESH_PLANE')
-        layout.operator("object.simulate", text="Simulate", icon='PLAY')
 
-class OBJECT_OT_AddActors(bpy.types.Operator):
+        # Add Actors Button
+        layout.operator("object.add_actors", text="Add Actors")
+
+        # Play Simulation Button
+        layout.operator("screen.play_simulation", text="Play Simulation")
+
+# Operator to add actors
+class AddActorsOperator(bpy.types.Operator):
     bl_idname = "object.add_actors"
-    bl_label = "Add Blood Simulation Actors"
-    
+    bl_label = "Add Actors"
+
     def execute(self, context):
-#       self.create_canvas()
-        self.create_brush()
+        add_actors(self, context)
         return {'FINISHED'}
-    
-    def create_canvas(self):
-        bpy.ops.mesh.primitive_plane_add(size=5)
-        canvas = bpy.context.object
-        canvas.name = "Canvas"
-        
-        # Add Dynamic Paint Canvas
-        mod = canvas.modifiers.new(name="Dynamic Paint", type='DYNAMIC_PAINT')
-        mod.ui_type = 'CANVAS'
-        bpy.ops.dpaint.type_toggle(type='CANVAS')
 
-        # Get the surface list and configure the paint settings
-        canvas_settings = mod.canvas_settings
-        surface = canvas_settings.surface_list.new()
-        surface.surface_type = 'PAINT'
-        surface.use_dry = True  # Persistent stains
-        surface.dry_speed = 0.1  # Slower drying for visible blood stains
-        surface.color_dry = (0.5, 0, 0)  # Dark red stain
-        surface.effect_ui = 'SPREAD'  # Helps spread the blood slightly
-        
-        print("Canvas created")
-
-    def create_brush(self):
-        bpy.ops.mesh.primitive_plane_add(size=1, location=(0, 0, 2))
-        brush = bpy.context.object
-        brush.name = "Brush"
-
-        # Add Particle System
-        ps = brush.modifiers.new(name="Blood Particles", type='PARTICLE_SYSTEM')
-        psys = brush.particle_systems.active
-        settings = psys.settings
-        settings.count = 150  # More blood splat particles
-        settings.lifetime = 30
-        settings.frame_start = 1
-        settings.frame_end = 5
-        settings.physics_type = 'NEWTON'
-        settings.render_type = 'NONE'
-        settings.emit_from = 'FACE'
-        settings.normal_factor = -2  # Controls blood splatter direction
-        settings.tangent_factor = 1  # Adds slight side motion for variety
-        settings.particle_size = 0.08
-        settings.use_rotations = True
-        settings.rotation_factor_random = 1.0  # Random splatter rotation
-
-        # Add Dynamic Paint Brush
-        mod = brush.modifiers.new(name="Dynamic Paint", type='DYNAMIC_PAINT')
-        mod.ui_type = 'BRUSH'
-        bpy.ops.dpaint.type_toggle(type='BRUSH')
-
-        brush_settings = mod.brush_settings
-        brush_settings.paint_color = (1, 0, 0)  # Blood red
-        brush_settings.paint_source = 'PARTICLE_SYSTEM'
-        brush_settings.particle_system = psys
-
-        print("Brush created")
-
-class OBJECT_OT_Simulate(bpy.types.Operator):
-    bl_idname = "object.simulate"
+# Operator to play simulation
+class PlaySimulationOperator(bpy.types.Operator):
+    bl_idname = "screen.play_simulation"
     bl_label = "Play Simulation"
 
     def execute(self, context):
-        bpy.ops.screen.animation_play()
+        play_simulation(self, context)
         return {'FINISHED'}
 
+# Register classes
 def register():
-    bpy.utils.register_class(BLOOD_SPLATTER_PT_Panel)
-    bpy.utils.register_class(OBJECT_OT_AddActors)
-    bpy.utils.register_class(OBJECT_OT_Simulate)
+    bpy.utils.register_class(SimplePanel)
+    bpy.utils.register_class(AddActorsOperator)
+    bpy.utils.register_class(PlaySimulationOperator)
 
+# Unregister classes
 def unregister():
-    bpy.utils.unregister_class(BLOOD_SPLATTER_PT_Panel)
-    bpy.utils.unregister_class(OBJECT_OT_AddActors)
-    bpy.utils.unregister_class(OBJECT_OT_Simulate)
+    bpy.utils.unregister_class(SimplePanel)
+    bpy.utils.unregister_class(AddActorsOperator)
+    bpy.utils.unregister_class(PlaySimulationOperator)
 
 if __name__ == "__main__":
     register()
